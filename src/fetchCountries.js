faa6dc3a828abd8883aead293c173f8e28bc4670
Jacek Pasierb <jack_byk@o2.pl>
2023-03-15 23:52:54 +0100

    fetch Country with http and created new object with country

---
+++ src/fetchCountries.js
@@ -0,0 +1,38 @@
+export function fetchCountries(name) {
+  const fetchCountry = fetch('https://restcountries.com/v3.1/all');
+
+  console.log('FetchCountry', fetchCountry);
+
+  fetchCountry
+    .then(response => {
+      console.log('response:', response);
+      const countryData = response.json();
+      console.log('countryData:', countryData);
+      return countryData;
+    })
+    .then(receiveCountryData => {
+      console.log('receiveCountryData:', receiveCountryData);
+
+      const selectedKeys = [
+        'name',
+        'capital',
+        'population',
+        'flags.svg',
+        'languages',
+      ];
+      let newCountryData = [];
+
+      for (const object of receiveCountryData) {
+        let newObject = {};
+        for (let key in object) {
+          if (selectedKeys.includes(key)) {
+            newObject[key] = object[key];
+          }
+        }
+
+        newCountryData.push(newObject);
+      }
+
+      console.log('to', newCountryData[5].name.official);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries.js';
 const DEBOUNCE_DELAY = 300;
+
+
+fetchCountries();
