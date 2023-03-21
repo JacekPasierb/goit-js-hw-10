f4cef547ff1fdb796161e64ddda8be4f1b57e7d8
Jacek Pasierb <jack_byk@o2.pl>
2023-03-21 20:50:57 +0100

    Created all new code for task

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 const inputSearchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
-const countryListItem = document.querySelectorAll('.item');
-const countryInfo = document.querySelectorAll('.info');
+const countryInfo = document.querySelector('.country-info');
 let countryListHtml = '';
 let countryInfoHtml = '';
 
@@ -16,10 +15,68 @@ inputSearchBox.addEventListener(
   'input',
   debounce(e => {
     let countryName = trimInput(inputSearchBox.value);
-    
-    fetchCountry(countryName).then(countryData => {
-      console.log('1:', countryData);
+    if (countryName === '') {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      return;
+    }
+    fetchCountry(countryName)
+      .then(countryData => {
+        countryListHtml = '';
+        countryInfoHtml = '';
+        if (countryData.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (countryData.length >= 2 && countryData.length <= 10) {
+          countryInfoHtml = '';
+          countryInfo.innerHTML = countryInfoHtml;
 
-    })
-  }, DEBOUNCE_DELAY))
+          countryData.forEach(country => {
+            countryListHtml += `<li class="item"><img src="${country.flags.svg}" width="60" height="40"> <span class="nameCountryList"> ${country.name.common}</span></li>`;
+          });
+        } else {
+          countryData.forEach(country => {
+            for (const key in country) {
+              if (key == 'languages') {
+                countryInfoHtml += `<span class="info"><b>${key}:</b> ${Object.values(
+                  country[key]
+                ).join(', ')}</span>`;
+              } else if (key == 'flags') {
+                countryInfoHtml += `<img src="${country.flags.svg}" width="60" height="40" style=" margin-right:20px;">`;
+              } else if (key == 'name') {
+                countryInfoHtml += `<span class="nameCountry"><b>${country.name.common}</b></span>`;
+              } else {
+                countryInfoHtml += `<p class="info"><b>${key}:</b> ${country[key]}</p>`;
+              }
+            }
+            const countryList = document.querySelectorAll('.item');
+            countryList.forEach(country => country.remove());
+            countryInfo.innerHTML = countryInfoHtml;
+            const spn = document.querySelector('.nameCountry');
+            spn.style.fontSize = '48px';
+            spn.style.fontWeight = 'bold';
+          });
+        }
 
+        countryList.innerHTML = countryListHtml;
+        const ul = document.querySelector('ul');
+        ul.style.padding = '0px';
+        const allLi = document.querySelectorAll('li');
+        allLi.forEach(li => {
+          li.style.display = 'flex';
+          li.style.alignItems = 'center';
+          li.style.gap = '20px';
+          li.style.marginBottom = '10px';
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        countryInfoHtml = '';
+        countryInfo.innerHTML = countryInfoHtml;
+        countryListHtml = '';
+        countryList.innerHTML = countryListHtml;
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }, DEBOUNCE_DELAY)
+);
--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,6 +1,5 @@
 export const fetchJsonToJs = response => {
   if (!response.ok) {
-    
     throw new Error(response.status);
   } else {
     return response.json();
